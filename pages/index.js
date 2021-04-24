84fff832ec59660da623d8fc9e0253cbaac3a78a
Aswin V <vaswin91@gmail.com>
2021-04-25 01:24:01 +0530

    Initial commit for career-recce

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,66 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} flx flx-col flx-cntr`}>
       <Head>
-        <title>Create Next App</title>
+        <title>👀</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={`${styles.main} flx flx-cntr flx-col w100`}>
+        <div className={`${styles.containerIframe} w100`}>
+          <iframe
+            className={`${styles.respIframe} w100 h100`}
+            loading="lazy"
+            src="https://www.spotifyjobs.com/locations/mumbai"
+            onLoad={() => console.log(`Iframe loaded`)}
+            sandbox="allow-scripts allow-same-origin allow-popups"
+            onError={(err) => {
+              console.error(`Iframe load error`);
+              console.table(err);
+            }}
+          ></iframe>
+        </div>
+        <div className={`${styles.controls} flx flx-cntr`}>
+          <div className="flx flx-col flx-cntr">
+            <button
+              type="button"
+              className={styles.btn}
+              aria-labelledby="btn-1"
+            >
+              S
+            </button>
+            <span id="btn-1" className={styles.lbl}>
+              Spotify
+            </span>
+          </div>
+          <div className="flx flx-col flx-cntr">
+            <button
+              type="button"
+              className={styles.btn}
+              aria-labelledby="btn-2"
+            >
+              L
+            </button>
+            <span id="btn-2" className={styles.lbl}>
+              Loom
+            </span>
+          </div>
+          <div className="flx flx-col flx-cntr">
+            <button
+              type="button"
+              className={styles.btn}
+              aria-labelledby="btn-3"
+            >
+              E
+            </button>
+            <span id="btn-3" className={styles.lbl}>
+              Elastic
+            </span>
+          </div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
