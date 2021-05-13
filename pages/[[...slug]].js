3f05c5abed1db064f3c2b2bc2099f6c883f355fa
Aswin V <vaswin91@gmail.com>
2021-05-13 18:08:27 +0530

    Refactor

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.v2.module.css";
-import { screenshotPage } from "lib/crawler";
+import { screenshotPage } from "lib/screenshot";
 
 const trackedJobs = [
   {
