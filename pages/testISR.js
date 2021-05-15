fc461f4027c5aa3f202cc83990697b57c5146d6b
Aswin V <vaswin91@gmail.com>
2021-05-15 21:27:05 +0530

    test ISR

---
+++ pages/testISR.js
@@ -0,0 +1,16 @@
+export default function TestISR({ esEpoch }) {
+  return (
+    <time style={{ color: "yellow" }}>
+      {esEpoch ? new Date(esEpoch).toLocaleString() : "No date received"}
+    </time>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      esEpoch: new Date().getTime(),
+    },
+    revalidate: 1,
+  };
+}
