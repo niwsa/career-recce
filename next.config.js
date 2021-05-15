85f45ebcc8ad6331aec5e843b07796c24ef2ff9c
Aswin V <vaswin91@gmail.com>
2021-05-15 18:08:43 +0530

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
-
 module.exports = {
-  future: {
-    webpack5: false,
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
